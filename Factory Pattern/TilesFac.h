e487aba8d2753e6c120b01e4246d0ff867aa8743
AriPoetra <100426610+AriPoetra@users.noreply.github.com>
2022-04-12 02:57:05 +0700

    Add files via upload

---
+++ Factory Pattern/TilesFac.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Tiles.h"
+
+class TilesFactory
+{
+public:
+	Tile* getTile(string type)
+	{
+		if (type == "ord")
+		{
+			return new OrdinaryTile();
+		}
+		else if (type == "tnt")
+		{
+			return new TntTile();
+		}
+		else if (type == "roc")
+		{
+			return new RocketTile();
+		}
+	}
+};
+#pragma once
